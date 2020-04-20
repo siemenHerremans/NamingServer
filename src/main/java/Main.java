c51ddb22346f419fd6c58b152cd392a96021146a
ArneTroch99 <arnetroch@gmail.com>
2020-04-20 10:59:16 +0000

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         NetworkManager networkManager = new NetworkManager("228.5.6.7", 6789);
-        Tester tester = new Tester("228.5.6.7", 8081);
+        Tester tester = new Tester("228.5.6.7", 6789);
         networkManager.run();
         tester.test();
 
