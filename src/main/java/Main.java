885b01eba698e54fceec9069299f2d66bf4b090f
BasCalder5 <62661069+BasCalder5@users.noreply.github.com>
2020-04-27 10:47:25 +0200

    added multithreading

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        NetworkManager networkManager = new NetworkManager("228.5.6.7", 6789);
+        Thread networkManager = new Thread(new NetworkManager("228.5.6.7", 6789));
         Tester tester = new Tester("228.5.6.7", 6789);
-        networkManager.run();
+        networkManager.start();
         tester.test();
 
     }
