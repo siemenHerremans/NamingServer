6a8d91ae8e78c3cab8ac4b407b3265ae18f71c8a
BasCalder5 <62661069+BasCalder5@users.noreply.github.com>
2020-04-27 11:59:59 +0200

    Added node.sndMsg()

--- src/main/java/Node/Main.java
+++ src/main/java/Node/Main.java
@@ -2,6 +2,11 @@ package Node;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("------Running node------");
 
+        String address = "228.5.6.7";
+        int socket = 6789;
+
+        Node node = new Node(address, socket);
     }
 }
--- src/main/java/Node/Node.java
+++ src/main/java/Node/Node.java
@@ -9,10 +9,30 @@ public class Node {
 
     private String groupAddress;
     private int socket;
+    private String IP;
+    private String name;
 
     public Node(String groupAddress, int socket) {
         this.groupAddress = groupAddress;
         this.socket = socket;
+        this.setHost();
+    }
+
+    public void setHost() {
+        try {
+            InetAddress host = InetAddress.getLocalHost();
+            IP = host.getHostAddress();
+            name = host.getHostName();
+
+            System.out.println("Ip: "+IP+"\nName: "+name);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String getIP() {
+        return IP;
     }
 
     private boolean sendMsg(String msg) {
@@ -25,8 +45,7 @@ public class Node {
             MulticastSocket s = new MulticastSocket(socket);
 
             s.joinGroup(group);
-            DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(),
-                    group, socket);
+            DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), group, socket);
             s.send(packet);
 
             s.leaveGroup(group);
