569a6a5d3baa10d2f7fd90e33ff46c4fc0ae18d7
NicolasGrupping <nicolas.grupping@student.uantwerpen.be>
2020-04-29 15:55:03 +0200

    Update NamingServer.java

--- src/main/java/NamingServer/NamingServer.java
+++ src/main/java/NamingServer/NamingServer.java
@@ -39,10 +39,10 @@ public class NamingServer {
     public void addNode(String ip, String name) throws Exception {
         int hashVal = hash(name);
         NodeMap.put(hashVal, ip);
-        udpNumberOfNodes(hashVal, ip);
+        udpNumberOfNodes(ip);
     }
 
-    public void udpNumberOfNodes(int hash, String ip) throws Exception {
+    public void udpNumberOfNodes(String ip) throws Exception {
         DatagramSocket ds = new DatagramSocket();
         String str = Integer.toString(NodeMap.size());
         InetAddress ipDest = InetAddress.getByName(ip);
