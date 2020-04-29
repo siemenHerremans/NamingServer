5617ba05263e9954545623666cf95de2ecc07f32
NicolasGrupping <nicolas.grupping@student.uantwerpen.be>
2020-04-29 16:26:52 +0200

    Update NamingServerHandler.java

--- src/main/java/NamingServer/NamingServerHandler.java
+++ src/main/java/NamingServer/NamingServerHandler.java
@@ -11,10 +11,10 @@ public class NamingServerHandler extends Thread {
     }
 
     public void run() {
-        String[] data = input.split("%");
-        System.out.println("ip: "+ data[0] +"\nName: "+ data[1]);
 
         try {
+            String[] data = input.split("%");
+            System.out.println("ip: "+ data[0] +"\nName: "+ data[1]);
             namingServer.addNode(data[0], data[1]);
             System.out.println("ip: " + data[0] + " name: " + data[1]);
 
