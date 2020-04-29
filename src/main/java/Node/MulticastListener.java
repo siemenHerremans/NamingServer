fe3272d3b6b48dbf6416ff3e7f4f83a2429b62b6
BasCalder5 <62661069+BasCalder5@users.noreply.github.com>
2020-04-29 15:12:14 +0200

    rare compile error

--- src/main/java/Node/MulticastListener.java
+++ src/main/java/Node/MulticastListener.java
@@ -21,30 +21,29 @@ public class MulticastListener implements Runnable {
     @Override
     public void run() {
         System.out.println("Multicast Listener running");
-        while(isRunning) {
-            try {
-                InetAddress group = InetAddress.getByName(groupAddress);
-                MulticastSocket s = new MulticastSocket(port);
-                s.joinGroup(group);
+        try {
+            InetAddress group = InetAddress.getByName(groupAddress);
+            MulticastSocket s = new MulticastSocket(port);
+            s.joinGroup(group);
 
-                byte[] buf = new byte[1000];
+            while (isRunning) {
+                byte[] buf = new byte[32768];
                 DatagramPacket recv = new DatagramPacket(buf, buf.length);
 
                 s.receive(recv);
-                s.leaveGroup(group);
 
                 String input = new String(recv.getData());
-                String[] data = input.split("%");
-
-
-                System.out.println("ip: " + data[0] + " name: " + data[1]);
-                currentNode.process(data[0], data[1]);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                isRunning = false;
+                MulticastHandler handler = new MulticastHandler(input, currentNode);
+                handler.start();
             }
+            s.leaveGroup(group);
+
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+            isRunning = false;
         }
+
     }
 
     public void stop() {
