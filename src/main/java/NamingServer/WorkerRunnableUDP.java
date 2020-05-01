e6e9ccf82a3ccd023ff655379b5a67c7c13f0106
siemenHerremans <siemen_herremans@hotmail.com>
2020-05-01 12:51:31 +0200

    Multithread UDP try 1

---
+++ src/main/java/NamingServer/WorkerRunnableUDP.java
@@ -0,0 +1,42 @@
+package NamingServer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class WorkerRunnableUDP implements Runnable{
+
+    DatagramSocket s = null;
+    int connectionAmount;
+    UDPListenerServer server;
+    NamingServer namingServer;
+
+    public WorkerRunnableUDP(DatagramSocket socket, int connectionAmount, UDPListenerServer server, NamingServer namingServer) {
+        this.s = socket;
+        this.connectionAmount = connectionAmount;
+        this.server = server;
+        this.namingServer = namingServer;
+    }
+
+    @Override
+    public void run() {
+
+        byte[] buf = new byte[32768];
+        DatagramPacket recv = new DatagramPacket(buf, buf.length);
+        try {
+            s.receive(recv);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String input = new String(recv.getData());
+        System.out.println("Unicast: " + input);
+        try {
+            namingServer.udpProcess(input, "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
